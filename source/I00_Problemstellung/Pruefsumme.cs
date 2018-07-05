4eca5d2644692d4be35e6670bc6269ad0e7add21
Uli Armbruster <ua@co-IT.eu>
2018-07-05 02:16:36 +0200

    Problemstellung hinzugefügt

---
+++ source/I00_Problemstellung/Pruefsumme.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Pruefsumme.I00_Problemstellung
+{
+  public static class Pruefsumme
+  {
+    //https://de.wikipedia.org/wiki/IP-Paket#Header_Checksum
+    //https://de.wikipedia.org/wiki/Pr%C3%BCfsumme
+    //https://de.wikipedia.org/wiki/Zyklische_Redundanzpr%C3%BCfung aka CRC
+    public static int Erzeuge(string ip)
+    {
+      if (!long.TryParse(ip.Replace(".", string.Empty), out var zahl))
+      {
+        throw new ArgumentOutOfRangeException(nameof(ip));
+      }
+
+      var summe = 0;
+      var istUngeradePosition = true;
+
+      while (zahl > 0)
+      {
+        var ziffer = (int)(zahl % 10);
+        if (istUngeradePosition)
+          summe += 5 * ziffer;
+        else
+          summe += ziffer;
+
+        zahl /= 10;
+        istUngeradePosition = !istUngeradePosition;
+      }
+
+      var modulo = summe % 9;
+      return modulo;
+    }
+
+  }
+}
--- source/Program.cs
+++ source/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Pruefsumme
+﻿using System;
+
+namespace Pruefsumme
 {
-  internal class Program
+  internal static class Program
   {
-    private static void Main(string[] args)
+    private static void Main()
     {
+      Func<string, int> prüfzifferBerechnung = I00_Problemstellung.Pruefsumme.Erzeuge;
+
+      var ip = "192.168.115.1";
+      Console.WriteLine($"Prüfziffer zur IP {ip} lautet: {prüfzifferBerechnung(ip)}");
+
+      ip = "10.10.148.1";
+      Console.WriteLine($"Prüfziffer zur IP {ip} lautet: {prüfzifferBerechnung(ip)}");
+
+      Console.ReadKey();
     }
   }
 }
